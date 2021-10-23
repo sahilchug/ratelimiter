2679e63fa7607a4b01ca13f4d9359be15398b18f
Sahil <chugdjango@gmail.com>
2021-10-23 13:44:09 -0700

    update code to handle signals

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"math/rand"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -14,7 +18,7 @@ const (
 type RateLimitedEntity string
 
 type RateLimiter interface {
-	Limit(entity RateLimitedEntity) (bool, error)
+	Limit(ctx context.Context, entity RateLimitedEntity) (bool, error)
 }
 
 type FixedRateLimiter struct {
@@ -22,21 +26,23 @@ type FixedRateLimiter struct {
 	currentLimit int
 	fillInterval time.Duration
 
-	closeCh chan bool
+	doneCh chan bool
 	sync.Mutex
+	ctx context.Context
 }
 
-func NewFixedRateLimiter(maxLimit int, fillInterval time.Duration) *FixedRateLimiter {
-	closeCh := make(chan bool, 1)
+func NewFixedRateLimiter(ctx context.Context, maxLimit int, fillInterval time.Duration) *FixedRateLimiter {
+	doneCh := make(chan bool, 1)
 
 	return &FixedRateLimiter{
 		maxLimit:     maxLimit,
 		fillInterval: fillInterval,
-		closeCh:      closeCh,
+		doneCh:       doneCh,
+		ctx:          ctx,
 	}
 }
 
-func (f *FixedRateLimiter) Limit(entity RateLimitedEntity) (bool, error) {
+func (f *FixedRateLimiter) Limit(ctx context.Context, entity RateLimitedEntity) (bool, error) {
 	f.Lock()
 	defer f.Unlock()
 	if f.currentLimit <= f.maxLimit {
@@ -46,42 +52,65 @@ func (f *FixedRateLimiter) Limit(entity RateLimitedEntity) (bool, error) {
 	return false, nil
 }
 
-func (f *FixedRateLimiter) Start() error {
-	log.Println(f.fillInterval)
-	ticker := time.NewTicker(time.Duration(f.fillInterval) * time.Second)
+func (f *FixedRateLimiter) Start(ctx context.Context) error {
+	log.Printf("fill interval set to %d seconds", f.fillInterval)
+	ticker := time.NewTicker(f.fillInterval)
 	defer ticker.Stop()
 
 	go func() {
-		for range ticker.C {
-			log.Println("filling tokens")
-			f.Lock()
-			log.Println("filling tokens: aquired lock")
-			f.currentLimit = 0
-			log.Println("filled tokens")
-			f.Unlock()
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("filling tokens")
+				f.Lock()
+				log.Println("filling tokens: aquired lock")
+				f.currentLimit = 0
+				log.Println("filled tokens")
+				f.Unlock()
+			case <-ctx.Done():
+				log.Println("context is done")
+				f.doneCh <- true
+			}
 		}
 	}()
 
-	<-f.closeCh
+	<-f.doneCh
 	return nil
 }
 
-func (f *FixedRateLimiter) Stop() error {
-	close(f.closeCh)
+func (f *FixedRateLimiter) Stop(ctx context.Context) error {
+	close(f.doneCh)
 	return nil
 }
 
 func main() {
 
-	rateLimiter := NewFixedRateLimiter(10, 10)
+	// signal channel to listen for SIGINT and SIGTERM
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// context
+	ctx := context.Background()
+	ctx, cancelFunc := context.WithCancel(ctx)
+
+	rateLimiter := NewFixedRateLimiter(ctx, 10, time.Duration(10)*time.Second)
 	go func() {
-		rateLimiter.Start()
+		rateLimiter.Start(ctx)
 	}()
+
+	go func() {
+		<-sigs
+		log.Println("calling cancelFunc")
+		cancelFunc()
+		os.Exit(1)
+	}()
+
+	defer rateLimiter.Stop(ctx)
 
 	for i := 0; i <= MaxIterations; i++ {
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 
-		ok, err := rateLimiter.Limit("127.0.0.1")
+		ok, err := rateLimiter.Limit(ctx, "127.0.0.1")
 		if err != nil {
 			log.Fatal(err)
 		}
